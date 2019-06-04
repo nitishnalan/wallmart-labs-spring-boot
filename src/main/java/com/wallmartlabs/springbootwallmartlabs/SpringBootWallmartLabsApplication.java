7b2ab2db63720fe026f71a0e88ec19c4679d138c
nitishnalan <nxl170130@gmail.com>
2019-06-03 22:53:41 -0500

    Initial Project setup

---
+++ src/main/java/com/wallmartlabs/springbootwallmartlabs/SpringBootWallmartLabsApplication.java
@@ -0,0 +1,13 @@
+package com.wallmartlabs.springbootwallmartlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWallmartLabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWallmartLabsApplication.class, args);
+    }
+
+}
